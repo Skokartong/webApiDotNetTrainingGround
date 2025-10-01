7968f4bf14df7b4b6ce25f143e3a0eb155d6a810
Amanda Olving <amanda.olving@gmail.com>
2025-10-01 16:50:34 +0200

    Init

---
+++ Models/Developer.cs
@@ -0,0 +1,10 @@
+namespace webApiDotNetTrainingGround.Models
+{
+    public class Developer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public int Experience { get; set; }
+    }
+}
